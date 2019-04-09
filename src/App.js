63467f4aecb1a1c56891912e0a7490d4c111bbd7
Fernando De La Madrid <ferdelamad@gmail.com>
2019-04-08 23:22:11 -0700

    Add app tests

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
+const Title = ({ text }) => <div>{text}</div>;
 class App extends Component {
+  state = {
+    on: false,
+    input: ""
+  };
   render() {
     return (
       <div className="App">
@@ -11,6 +16,7 @@ class App extends Component {
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
+          <Title text="I am title" />
           <a
             className="App-link"
             href="https://reactjs.org"
@@ -19,10 +25,26 @@ class App extends Component {
           >
             Learn React
           </a>
+          <ul className="tyler">
+            <li>test</li>
+            <li>test</li>
+            <li>test</li>
+          </ul>
         </header>
+        <div>
+          <p className="button-state">{this.state.on ? "Yes!" : "No!"}</p>
+          <button onClick={() => this.setState({ on: true })}>Click</button>
+          <h2>{this.state.input}</h2>
+          <input
+            onChange={e => this.setState({ input: e.currentTarget.value })}
+            type="text"
+          />
+        </div>
       </div>
     );
   }
 }
+
+export const Link = props => <a href={props.address}>Click</a>;
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,73 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App, { Link } from "./App";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+import { configure, shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import toJson from "enzyme-to-json";
+
+configure({ adapter: new Adapter() });
+
+describe("<App />", () => {
+  it("should contain 1 p element", () => {
+    const wrapper = shallow(<App />);
+    //shallow(<App />, { context: {}, disableLifecycleMethods: bool})
+    expect(wrapper.find("p").length).toBe(1);
+  });
+
+  it("should contain className of App", () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find(".App").exists()).toBe(true);
+  });
+
+  // it("should contain a ul with 3 li", () => {
+  //   const wrapper = shallow(<App />);
+  //   expect(wrapper.find("ul").children.length).toBe(3);
+  // });
+
+  it("ul should have a className of tyler", () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find("ul").hasClass("tyler")).toBe(true);
+  });
+
+  //find by prop
+  // it("find element by prop", () => {
+  //   const wrapper = shallow(<App />);
+  //   expect(wrapper.find('[text="I am title"]').text()).toBe("I am title");
+  // });
+
+  //snapshot press U to re-run
+  it("matches the snapshot", () => {
+    const tree = shallow(<App />);
+    expect(toJson(tree)).toMatchSnapshot();
+  });
+
+  it("on button click changes p text", () => {
+    const wrapper = shallow(<App />);
+    const button = wrapper.find("button");
+    expect(wrapper.find(".button-state").text()).toBe("No!");
+    button.simulate("click");
+    expect(wrapper.find(".button-state").text()).toBe("Yes!");
+  });
+
+  it("on input change, title changes text", () => {
+    const wrapper = shallow(<App />);
+    const input = wrapper.find("input");
+    expect(wrapper.find("h2").text()).toBe("");
+    input.simulate("change", { currentTarget: { value: "Tyler" } });
+    expect(wrapper.find("h2").text()).toBe("Tyler");
+  });
+});
+
+describe("<Link />", () => {
+  it("link component accepts address prop", () => {
+    const wrapper = shallow(<Link address="www.google.com" />);
+    expect(wrapper.instance().props.address).toBe("www.google.com");
+  });
+
+  it("a tag node renders href correctly", () => {
+    const wrapper = shallow(<Link address="www.google.com" />);
+
+    expect(wrapper.props().href).toBe("www.google.com");
+  });
 });
